a4c8a8c25353e474775b5daf5caa27e1b7f45b34
ProfInvincible <muoghaluijeoma9@gmail.com>
2025-01-24 15:58:49 +0100

    m

---
+++ babel.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: ["babel-preset-expo"], // This is needed for Expo
+  plugins: [
+    "react-native-reanimated/plugin", // This plugin is commonly needed for React Native Reanimated
+  ],
+};
